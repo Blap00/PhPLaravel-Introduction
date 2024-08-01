674bdc0b7aaf75ca8ca002cfca647b064c6d62ab
Blap00 <fa.palma@duocuc.cl>
2024-08-01 16:29:42 -0400

    Setup our Index, layout files, controller and routes

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/index.blade.php -->
+@extends('layout.layout')
+
+<!-- Titulo -->
+@section('title', 'Acerca de')
+
+<!-- Main -->
+@section('content')
+    <h1>Index</h1>
+    <p>Site Post!.</p>
+@endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
+//GET Sites
+
+Route::get('/auth/login', function () {
+    return view('registration.login');
+})->name('login');
+Route::get('/auth/register', function () {
+    return view('registration.register');
+})->name('register');
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('index');
+
+//POST sites
+Route::post('/auth/login', function () {
+    return view('registration.login');
+});
+Route::post('/auth/register', [UserController::class, 'register'], function () {
+    return view('registration.register');
 });
